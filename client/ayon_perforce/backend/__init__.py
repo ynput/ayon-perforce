68ac2f7af2e586ce0fac4575d51bafd6d48ff155
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2025-03-12 20:36:39 +0100

    wip: fix rest/backend merge

--- client/ayon_perforce/backend/__init__.py
+++ client/ayon_perforce/backend/__init__.py
@@ -1,11 +1,13 @@
 """Wrapper for Perforce REST API and WebServer."""
-from communication_server import WebServer
-from rest_api import PerforceModuleRestAPI
-from rest_stub import PerforceRestStub
+from ayon_perforce.backend.communication_server import WebServer
+from ayon_perforce.backend.rest_api import PerforceModuleRestAPI
+from ayon_perforce.backend.rest_stub import PerforceRestStub
+from ayon_perforce.backend.backend import PerforceBackend
 
 
 __all__ = [
     "PerforceModuleRestAPI",
     "PerforceRestStub",
     "WebServer",
+    "PerforceBackend",
 ]
