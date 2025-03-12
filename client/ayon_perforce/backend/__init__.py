6105989c261395339dc195c01c7a4434cb15ed41
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2025-03-12 21:44:17 +0100

    Revert "wip: fix rest/backend merge"
    This reverts commit 68ac2f7af2e586ce0fac4575d51bafd6d48ff155.

--- client/ayon_perforce/backend/__init__.py
+++ client/ayon_perforce/backend/__init__.py
@@ -1,13 +1,11 @@
 """Wrapper for Perforce REST API and WebServer."""
-from ayon_perforce.backend.communication_server import WebServer
-from ayon_perforce.backend.rest_api import PerforceModuleRestAPI
-from ayon_perforce.backend.rest_stub import PerforceRestStub
-from ayon_perforce.backend.backend import PerforceBackend
+from communication_server import WebServer
+from rest_api import PerforceModuleRestAPI
+from rest_stub import PerforceRestStub
 
 
 __all__ = [
     "PerforceModuleRestAPI",
     "PerforceRestStub",
     "WebServer",
-    "PerforceBackend",
 ]
