069c11ffc2611d397a98a6ab11bde00ead008611
Ondrej Samohel <ondrej.samohel@ynput.io>
2026-08-12 17:21:03 +0200

    fix(api): fix NameError in forward references

--- client/ayon_perforce/api/models.py
+++ client/ayon_perforce/api/models.py
@@ -148,7 +148,13 @@ def _enable_alias_kwargs(cls: type[R]) -> type[R]:
         Modified class.
 
     """
-    type_hints = get_type_hints(cls)
+    try:
+        type_hints = get_type_hints(cls)
+    except NameError:
+        # This can happen if the dataclass has forward references.
+        # In that case, we can't do any type checking,
+        # so just return the class.
+        return cls
     datetime_fields = {
         name
         for name, annotation in type_hints.items()
